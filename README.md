7b3aa3c450a9f5e07d9a3d1897ec58270a477998
Andrew Tremblay <gloriousrocketship@gmail.com>
2025-06-29 16:12:22 -0400

    adding uv & subcategories

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Analyze Bank and Credit Card Statements into CSVs and Flow Diagrams.
 
 This software was written with Python 3.11.4. Anything with 3.11 should be fine.
 
-`pip install -r requirements.txt`
+`uv` is recommended for package installation and virtual environments. See <https://github.com/astral-sh/uv>
+
+`uv add -r requirements.txt`
 
 Install `gemma2:27b` with OllamaLLM (Ollama 0.1.26 or later).
 
@@ -14,7 +16,7 @@ Install `gemma2:27b` with OllamaLLM (Ollama 0.1.26 or later).
 
 1. Add your bank statements to the appropriate folder in the `/data` directory.
 
-2. Run your data directory `python analyze_pdf.py`
+2. Run your data directory `uv run analyze_pdf.py`
 
 Expected output is will be something you can add to [sankeymatic](https://sankeymatic.com/build/) and render.
 
@@ -25,3 +27,5 @@ Better run parameters (choose the llm for classification).
 Direct-to-sankeymatic integration.
 
 Automatically classify which pdf should belong to each data.
+
+[Docling integration](https://docling-project.github.io/docling/).
--- analyze_pdf.py
+++ analyze_pdf.py
@@ -1,3 +1,99 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "aiohttp==3.8.4",
+#     "aiosignal==1.3.1",
+#     "annotated-types==0.7.0",
+#     "anyio==4.7.0",
+#     "async-timeout==4.0.2",
+#     "attrs==23.1.0",
+#     "autopep8==2.0.2",
+#     "backoff==2.2.1",
+#     "black==23.3.0",
+#     "certifi==2023.5.7",
+#     "cffi==1.15.1",
+#     "cfgv==3.3.1",
+#     "charset-normalizer==3.2.0",
+#     "click==8.1.4",
+#     "dataclasses-json==0.5.7",
+#     "deprecation==2.0.7",
+#     "distlib==0.3.6",
+#     "distro==1.9.0",
+#     "et-xmlfile==1.1.0",
+#     "filelock==3.12.2",
+#     "frozenlist==1.3.3",
+#     "greenlet==3.1.1",
+#     "h11==0.14.0",
+#     "httpcore==1.0.7",
+#     "httpx>=0.27.0",
+#     "httpx-sse==0.4.0",
+#     "identify==2.5.24",
+#     "idna==3.4",
+#     "iniconfig==2.0.0",
+#     "jiter==0.8.2",
+#     "jsonpatch==1.33",
+#     "jsonpointer==3.0.0",
+#     "langchain==0.3.13",
+#     "langchain-community==0.3.13",
+#     "langchain-core==0.3.28",
+#     "langchain-ollama==0.2.2",
+#     "langchain-openai==0.2.14",
+#     "langchain-text-splitters==0.3.4",
+#     "langsmith==0.2.4",
+#     "marshmallow==3.19.0",
+#     "marshmallow-enum==1.5.1",
+#     "monotonic==1.6",
+#     "multidict==6.0.4",
+#     "mypy==1.3.0",
+#     "mypy-extensions==1.0.0",
+#     "nodeenv==1.8.0",
+#     "numpy==1.26.4",
+#     "ollama==0.4.4",
+#     "openai==1.58.1",
+#     "openpyxl==3.1.1",
+#     "orjson==3.10.12",
+#     "packaging==24.2",
+#     "pandas==2.2.3",
+#     "pathspec==0.11.1",
+#     "pillow==10.0.0",
+#     "platformdirs==3.8.1",
+#     "pluggy==1.2.0",
+#     "pre-commit==3.3.3",
+#     "pybind11==2.11.1",
+#     "pycairo==1.24.0",
+#     "pycodestyle==2.10.0",
+#     "pycparser==2.21",
+#     "pydantic==2.10.4",
+#     "pydantic-core==2.27.2",
+#     "pydantic-settings==2.7.0",
+#     "pygobject==3.44.1",
+#     "pypdf==5.1.0",
+#     "pytest==7.3.1",
+#     "python-dateutil==2.8.2",
+#     "python-dotenv==0.21.1",
+#     "pytz==2024.2",
+#     "pyyaml==6.0",
+#     "regex==2024.11.6",
+#     "requests==2.31.0",
+#     "requests-toolbelt==1.0.0",
+#     "rudder-sdk-python==2.0.2",
+#     "ruff==0.0.272",
+#     "six==1.16.0",
+#     "sniffio==1.3.1",
+#     "sqlalchemy==2.0.36",
+#     "tenacity==9.0.0",
+#     "termcolor==2.3.0",
+#     "tiktoken==0.8.0",
+#     "tqdm==4.65.0",
+#     "typer==0.9.0",
+#     "typing-extensions==4.12.2",
+#     "typing-inspect==0.9.0",
+#     "tzdata==2024.2",
+#     "urllib3==2.0.3",
+#     "virtualenv==20.23.1",
+#     "yarl==1.9.2",
+# ]
+# ///
 from pprint import pprint
 from langchain_openai import ChatOpenAI
 from langchain_ollama import OllamaLLM
@@ -50,8 +146,8 @@ def main():
     categorize_all_pdfs_in_folder("data/barclays", extract_barclays_transactions, categorize_barclays)
     categorize_all_pdfs_in_folder("data/paypal", extract_paypal_transactions, categorize_paypal)
 
-    csvs = []
     # then gather all csvs that the pdfs generated
+    csvs = []
     boa_csvs = all_csvs_in_folder("data/boa_cc")
     csvs.extend(boa_csvs)
     schwab_csvs = all_csvs_in_folder("data/schwab")
@@ -62,24 +158,24 @@ def main():
     csvs.extend(paypal_csvs)
 
     # now get the csvs of the month you want
-    relevant_csvs = [csv for csv in csvs if "Jan-2025" in csv or "2025-01" in csv]
+    # relevant_csvs = [csv for csv in csvs if "Jan-2025" in csv or "2025-01" in csv]
     # or just get all of them
-    # relevant_csvs = csvs
-    # pprint(csvs_of_month)
+    relevant_csvs = csvs
     rollup_csv = []
     data = {}
     for csv_file_path in relevant_csvs:
         csv_data_df = read_csv(csv_file_path)
-        rollup_csv.extend(csv_data_df.to_dict('records'))
+        records = csv_data_df.to_dict('records')
+        for record in records:
+            record['source'] = csv_file_path
+        rollup_csv.extend(records)
         print(f'\n\n\nProcessing {csv_file_path}\n{csv_data_df}\n')
         data = count_categories(csv_data_df, data)
     # Write csv to file
     export_to_csv(rollup_csv, 'rollup.csv')
     print("\n")
+    # ouput sankeymatic for copying
     print(fmt_sankeymatic(data))
-    # pdf_file = "data/paypal/statement-Nov-2024.pdf"
-    # pdf_file = "data/paypal/statement-Oct-2024.pdf"
-    # categorize_pdf_to_csv(pdf_file, extract_paypal_transactions, categorize_paypal)
 
 if __name__ == "__main__":
     main()
